763a4332e9ab1e119c12eb4399489a912d7bf193
MINH BO <minhbo0000@gmail.com>
2018-01-27 12:25:49 -0800

    Networking spawn position

--- DarkImpulse/Assets/Script/Game/SetupLocalPlayer.cs
+++ DarkImpulse/Assets/Script/Game/SetupLocalPlayer.cs
@@ -17,8 +17,17 @@ public class SetupLocalPlayer : NetworkBehaviour
     private GameObject detectionComponent;
     [SerializeField]
     private GameObject locationNotification;
+    [SerializeField]
+    private float minBoxBound;
+    [SerializeField]
+    private float maxBoxBound;
+    [SerializeField]
+    private float sphereCastRadius;
 
-    private int SeekerFlag = 1;
+
+    private int SeekerFlag         = 1;
+    private Vector3 seekerPosition = new Vector3(0, 0, 0);
+
 
     // Use this for initialization
     void Start()
@@ -37,6 +46,8 @@ public class SetupLocalPlayer : NetworkBehaviour
             }
             else
                 ManageComponent(locationNotification);
+
+            SetLocalPlayerPosition();
         }
     }
 
@@ -58,5 +69,50 @@ public class SetupLocalPlayer : NetworkBehaviour
             return;
         }
         component.SetActive(true);
+    }
+
+
+
+    private void SetLocalPlayerPosition()
+    {
+        if (IsSeeker())
+        {
+            transform.position = seekerPosition;
+            return;
+        }
+
+        //TODO:
+        //Set a random point around the seeker
+        bool hasEmptyLocation = false;
+        while (!hasEmptyLocation)
+        {
+            var randPosition = RandomPointInLevel();
+            hasEmptyLocation = IsEmpty(randPosition);
+            if (hasEmptyLocation)
+            {
+                transform.position = randPosition;
+                return;
+            }
+        }
+        
+    }
+
+
+    private bool IsEmpty(Vector3 castAtLocation)
+    {
+        Ray ray = new Ray(castAtLocation, Vector3.zero);
+        RaycastHit hitInfo;
+        bool didHit = Physics.SphereCast(ray, sphereCastRadius, out hitInfo);
+        if (didHit)
+            print(hitInfo.collider.name);
+        return !didHit;
+    }
+
+
+    private Vector3 RandomPointInLevel()
+    {
+        float randX = Random.Range(minBoxBound, maxBoxBound);
+        float randY = Random.Range(minBoxBound, maxBoxBound);
+        return new Vector3(randX, randY, 0);
     }
 }
