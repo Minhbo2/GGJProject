ac804134ba215c3395c0c41fbcce59cfbf01a0c4
zgreathouse <greathouse.zachary@gmail.com>
2018-01-26 22:15:11 -0800

    add movement to playercontroller.cs script

--- GGJ_Unity/Assets/Script/Player/PlayerController.cs
+++ GGJ_Unity/Assets/Script/Player/PlayerController.cs
@@ -4,13 +4,53 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	Rigidbody m_RigidBody;
+
+	float m_MoveSpeed;
+	void Awake () 
+	{
+		if (!m_RigidBody) 
+		{
+			m_RigidBody = gameObject.GetComponent<Rigidbody> ();
+		}
+
+	}
+
 	// Use this for initialization
 	void Start () {
-		
+		m_MoveSpeed = 1.0f;
+		m_RigidBody = gameObject.GetComponent<Rigidbody> ();
+
+			
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+
+		if (Input.GetKey(KeyCode.W))
+		{
+			m_RigidBody.velocity += Vector3.forward * m_MoveSpeed;
+		}
+
+		if (Input.GetKey(KeyCode.S)) 
+		{
+			m_RigidBody.velocity += Vector3.back * m_MoveSpeed;
+		}
+
+		if (Input.GetKey(KeyCode.A)) 
+		{
+			m_RigidBody.velocity += Vector3.left * m_MoveSpeed;
+		}
+
+		if (Input.GetKey(KeyCode.D)) 
+		{
+			m_RigidBody.velocity += Vector3.right * m_MoveSpeed;
+		}
+		if (Input.GetKey (KeyCode.Space)) 
+		{
+			Debug.Log ("ASDASD");
+
+			m_RigidBody.velocity += Vector3.up * m_MoveSpeed;
+		}
 	}
 }
