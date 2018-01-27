3977bd833d8dd4f2ab9c77a0a33abb95cbb213af
SonnySN <35589142+SonnySN@users.noreply.github.com>
2018-01-27 01:19:23 -0800

    Added Max Speed and rearranged code
    Added red mat for visual purposes in player movement

--- GGJ_Unity/Assets/Script/Player/PlayerController.cs
+++ GGJ_Unity/Assets/Script/Player/PlayerController.cs
@@ -6,48 +6,51 @@ public class PlayerController : MonoBehaviour {
 
 	Rigidbody m_RigidBody;
 
-	float m_MoveSpeed;
-	void Awake () 
-	{
-		if (!m_RigidBody) 
-		{
-			m_RigidBody = gameObject.GetComponent<Rigidbody> ();
-		}
+	public float m_MoveSpeed;
 
-	}
-
+    public float m_MaxSpeed;
 	// Use this for initialization
 	void Start () {
 		m_MoveSpeed = 1.0f;
-		m_RigidBody = gameObject.GetComponent<Rigidbody> ();
+        m_MaxSpeed = 250;
+		m_RigidBody = GetComponent<Rigidbody> ();
 
-	}
+        if (!m_RigidBody)
+        {
+           m_RigidBody = gameObject.AddComponent<Rigidbody>() as Rigidbody;
+        }
+
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 
-		if (Input.GetKey(KeyCode.W))
-		{
-			m_RigidBody.velocity += Vector3.forward * m_MoveSpeed;
-		}
+        if (m_RigidBody.velocity.sqrMagnitude < m_MaxSpeed)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                m_RigidBody.velocity += Vector3.forward * m_MoveSpeed;
+            }
 
-		if (Input.GetKey(KeyCode.S)) 
-		{
-			m_RigidBody.velocity += Vector3.back * m_MoveSpeed;
-		}
+            if (Input.GetKey(KeyCode.S))
+            {
+                m_RigidBody.velocity += Vector3.back * m_MoveSpeed;
+            }
 
-		if (Input.GetKey(KeyCode.A)) 
-		{
-			m_RigidBody.velocity += Vector3.left * m_MoveSpeed;
-		}
+            if (Input.GetKey(KeyCode.A))
+            {
+                m_RigidBody.velocity += Vector3.left * m_MoveSpeed;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                m_RigidBody.velocity += Vector3.right * m_MoveSpeed;
+            }
+            Debug.Log(m_RigidBody.velocity.sqrMagnitude);
 
-		if (Input.GetKey(KeyCode.D)) 
-		{
-			m_RigidBody.velocity += Vector3.right * m_MoveSpeed;
-		}
-		if (Input.GetKey (KeyCode.Space)) 
-		{
-			m_RigidBody.velocity += Vector3.up * m_MoveSpeed;
-		}
+        }
+
+
+
+		
 	}
 }
