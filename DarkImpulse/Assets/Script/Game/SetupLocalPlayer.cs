ef6024e066e3e2b3c2e1006a608f922631a1a5a9
MINH BO <minhbo0000@gmail.com>
2018-01-26 23:20:19 -0800

    Tag Mechanic and Testing Networking

---
+++ DarkImpulse/Assets/Script/Game/SetupLocalPlayer.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class SetupLocalPlayer : NetworkBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            print(this.transform.name + " is local player");
+            //TODO: 
+            //enable movement script
+        }
+    }
+
+}
---
+++ DarkImpulse/Assets/Script/Player/TagComponent.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class TagComponent : MonoBehaviour {
+
+    [SerializeField]
+    private float expandSweepByAmound;
+
+    private CircleCollider2D sweepCircle;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag != "Hider")
+            return;
+
+        print("Found a hider!");
+        //TODO: 
+        // expand the radius of the circle 
+        ExpandSweepArea();
+        SetHiderPositionNotification();
+        //Grab the hider position notifier and activate it
+    }
+
+
+    private void ExpandSweepArea()
+    {
+        if (HasSweepCircle())
+            sweepCircle.radius += expandSweepByAmound;
+    }
+
+
+    private bool HasSweepCircle()
+    {
+        sweepCircle = GetComponent<CircleCollider2D>();
+        return (sweepCircle != null);
+    }
+
+
+    private void SetHiderPositionNotification()
+    {
+
+    }
+}
